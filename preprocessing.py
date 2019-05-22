8effc4f4e6114969afec10ef188409ec923f4e88
flexai <alexandermartinez@Alexanders-MacBook-Pro.local>
2019-05-22 09:42:32 -0700

    Updated preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,7 @@
+import os
+from os import listdir
+from string import punctuation
+from collections import Counter
 from nltk.corpus import stopwords
 import string
 
@@ -32,9 +36,37 @@ def clean_doc(doc):
 
     return tokens
 
+def add_doc_to_vocab(filename, vocab):
+    # Load doc and add to vocab
+    doc = load_doc(filename)
+    tokens = clean_doc(doc)
+    vocab.update(tokens)
+
+def process_docs(directory, vocab, is_training):
+    # Load all docs in a directory
+    for filename in listdir(directory):
+        if is_training and filename.startswith('cv9'):
+            continue
+        if not is_training and not filename.startswith('cv9'):
+            continue
+
+        # Create the full path of the file to open
+        path = os.path.join(directory, filename)
+
+        add_doc_to_vocab(path, vocab)
+
+def save_list(lines, filename):
+    # Save list to file
+    data = '\n'.join(lines)
+    file = open(filename, 'w')
+    file.write(data)
+    file.close()
+
 
 if __name__=="__main__":
-    filename = ''
-    text = load_doc(filename)
-    tokens = clean_doc(text)
-    print(tokens)
+    vocab = Counter()
+    process_docs('txt_sentoken/neg', vocab, True)
+    process_docs('txt_sentoken/pos', vocab, True)
+    print(len(vocab))
+    print(vocab.most_common(50))
+    save_list(vocab, 'vocab.txt')
